9e1d44639e78336688039a656b47ecea16f3e9e5
ken <jitamm20081@gmail.com>
2025-05-27 23:24:32 +0800

    Initial Commit

---
+++ TodoList/TodoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoListApp.swift
+//  TodoList
+//
+//  Created by ken on 2025/5/27.
+//
+
+import SwiftUI
+
+@main
+struct TodoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
